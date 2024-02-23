f66df57d5eb84caeff913c6dce488c883700bdd1
Daniil Kolbasov <developer@daniil-kolbasov.ru>
2024-02-23 06:23:02 +0500

    Add project files.

---
+++ mindbox.ShapeLibrary/Circle.cs
@@ -0,0 +1,50 @@
+﻿namespace mindbox.ShapeLibrary;
+
+/// <summary>Represents the shape of a circle</summary>
+/// <exception cref="ArgumentException"></exception>
+public class Circle : Shape
+{
+    double _value;
+
+    /// <summary>Array of values</summary>
+    public double Value
+    {
+        get => _value;
+        init
+        {
+            if (value <= 0)
+                throw new ArgumentException($"The {nameof(_value)} value must be greater than or equal to zero", nameof(Value));
+            _value = value;
+        }
+    }
+
+    public Circle(double value) => Value = value;
+
+
+    /// <summary>Get the square of the circle.</summary>
+    /// <remarks>Default by radius.</remarks>
+    public sealed override double GetSquare() => GetSquareByRadius(Value);
+
+    /// <summary>The name of the square formula</summary>
+    public enum FomulaNames
+    {
+        Radius,
+        Diameter,
+        Perimeter
+    }
+
+    /// <summary>Get the square of the circle by the fomula name.</summary>
+    public double GetSquare(FomulaNames fomulaName) => fomulaName switch
+    {
+        FomulaNames.Radius => GetSquareByRadius(Value),
+        FomulaNames.Diameter => GetSquareByDiameter(Value),
+        FomulaNames.Perimeter => GetSquareByPerimeter(Value),
+        _ => throw new NotImplementedException()
+    };
+
+    internal static double GetSquareByRadius(double radius) => Math.PI * Math.Pow(radius, 2);
+
+    internal static double GetSquareByDiameter(double diameter) => 1 / 4 * Math.PI * Math.Pow(diameter, 2);
+
+    internal static double GetSquareByPerimeter(double perimeter) => Math.Pow(perimeter, 2) / 4 * Math.PI;
+}
---
+++ mindbox.ShapeLibrary/Triangle.cs
@@ -0,0 +1,46 @@
+﻿namespace mindbox.ShapeLibrary;
+
+/// <summary>
+/// Represents the shape of a triangle
+/// </summary>
+/// <exception cref="ArgumentException"></exception>
+public class Triangle : Shape
+{
+    double _sideA, _sideB, _sideC;
+    public double SideA
+    {
+        get => _sideA;
+        init => _sideA = ChackGreaterThanZero(value);
+    }
+    public double SideB
+    {
+        get => _sideB;
+        init => _sideB = ChackGreaterThanZero(value);
+    }
+    public double SideC
+    {
+        get => _sideC;
+        init => _sideC = ChackGreaterThanZero(value);
+    }
+    double ChackGreaterThanZero(double value)
+    {
+        if (value <= 0)
+            throw new ArgumentException("The sides of a triangle cannot be less than or equal to zero");
+        return value;
+    }
+
+    public Triangle(double sideA, double sideB, double sideC)
+    {
+        SideA = sideA;
+        SideB = sideB;
+        SideC = sideC;
+    }
+
+    public sealed override double GetSquare() => GetSuareByHeron(SideA, SideB, SideC);
+
+    internal static double GetSuareByHeron(double a, double b, double c)
+    {
+        double p = (a + b + c) / 2;
+        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
